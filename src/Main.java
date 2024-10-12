2b2c25ae4f5a23df5fa2060c8abb0d0e897ea27c
VladRyzhalovv <vlad.ryzhalov@mail.ru>
2024-10-12 21:54:50 +0300

    dev 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.nio.file.Files;
+
 public class Main {
     public static void main(String[] args) {
+        String folderPath = "C:\\Users\\Vlad\\Desktop\\Java";
+        File file = new File(folderPath);
 
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }

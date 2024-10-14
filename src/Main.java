7c48fbf76c78f71c13c9899214e4ce9b0d1a02f2
VladRyzhalovv <vlad.ryzhalov@mail.ru>
2024-10-14 21:25:21 +0300

    dev 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import java.io.File;
-import java.nio.file.Files;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "C:\\Users\\Vlad\\Desktop\\Java";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
     }
 
     public static long getFolderSize(File folder) {
